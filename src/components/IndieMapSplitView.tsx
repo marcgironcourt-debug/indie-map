29dd261c280d79d9aa0b8570206a7ab93e3d0c9e
Marcos <marcos@example.com>
2025-11-30 16:46:09 -0500

    Mobile UI: filtres repositionnés + ajustements sheet

--- src/components/IndieMapSplitView.tsx
+++ src/components/IndieMapSplitView.tsx
@@ -673,7 +673,7 @@ export default function IndieMapSplitView() {
         </div>
       </div>
 
-      <div className="absolute bottom-4 right-4 z-[1300] w-[min(380px,60vw)] indie-filter-bar">
+      <div className="absolute bottom-0 right-4 z-[1300] w-[min(380px,60vw)] indie-filter-bar">
         <FilterBar
           categories={categories}
           activeCategory={category}
